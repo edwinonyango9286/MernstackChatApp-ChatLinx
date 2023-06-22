5b4484056fbc14c4046d4be516c0197d9510c04b
edwin onyango <ookoedwin29@gmail.com>
2023-06-22 13:08:51 +0300

    Initial commit

---
+++ Git-commands.js
@@ -0,0 +1,113 @@
+// Initialize a new Git repository
+// git init
+
+// Clone a remote repository to your local machine
+// git clone <repository-url>
+
+// Add files to the staging area
+// git add <file1> <file2>
+
+// Commit changes with a descriptive message
+// git commit -m "Commit message"
+
+// Push local changes to a remote repository
+// git push origin <branch-name>
+
+// Pull latest changes from a remote repository
+// git pull origin <branch-name>
+
+// Create a new branch
+// git branch <branch-name>
+
+// Switch to a different branch
+// git checkout <branch-name>
+
+// Merge changes from one branch into another
+// git merge <branch-name>
+
+// View the commit history
+// git log
+
+// Show the status of your Git repository
+// git status
+
+// Discard changes in a file
+// git checkout -- <file>
+
+// Remove a file from the staging area
+// git reset <file>
+
+// Create a new remote repository
+// git remote add origin <repository-url>
+
+// Rename a branch
+// git branch -m <new-branch-name>
+
+// Delete a branch
+// git branch -d <branch-name>
+
+// Remove a remote repository
+// git remote remove origin
+
+// Fetch latest changes from a remote repository
+// git fetch origin
+
+// Create and checkout a new branch in one step
+// git checkout -b <new-branch-name>
+
+// List all branches
+// git branch
+
+// Show the changes made in a commit
+// git show <commit-hash>
+
+// Amend the last commit
+// git commit --amend
+
+// Revert a specific commit
+// git revert <commit-hash>
+
+// Create a new tag
+// git tag <tag-name>
+
+// List all tags
+// git tag
+
+// Switch to a specific tag
+// git checkout <tag-name>
+
+// Delete a tag
+// git tag -d <tag-name>
+
+// Create a new branch from a specific commit
+// git branch <new-branch-name> <commit-hash>
+
+// Rename a remote repository
+// git remote rename <old-name> <new-name>
+
+// Update the local repository with the latest changes from remote
+// git fetch
+
+// Fetch and merge changes from remote repository
+// git pull
+
+// Show the changes between two commits
+// git diff <commit1> <commit2>
+
+// Stash changes to work on a different branch
+// git stash
+
+// Apply the most recent stash
+// git stash apply
+
+// Show the list of stashes
+// git stash list
+
+// Discard the most recent stash
+// git stash drop
+
+// Rebase a branch onto another branch
+// git rebase <base-branch>
+
+// Squash multiple commits into one
+// git rebase -i HEAD~<number-of-commits>
